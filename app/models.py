6d2dd024675550f93a63cad20ecd763f21bfd8e0
andersonjbaptiste26 <andersonjbaptiste26@gmail.com>
2026-08-25 01:42:02 -0400

    Fix email column definition to enforce uniqueness

--- app/models.py
+++ app/models.py
@@ -140,4 +140,5 @@ class Odds(Base):
 class User(Base):
     __tablename__ = "users"
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    email = Column(String, unique
+    email = Column(String, unique=True)
+
