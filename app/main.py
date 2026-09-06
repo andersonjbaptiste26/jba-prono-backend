1fa42317167faff651d16c5fee067fbe522ca9eb
andersonjbaptiste26 <andersonjbaptiste26@gmail.com>
2026-09-06 03:10:09 -0400

    Add combo and ticket routers to main application

--- app/main.py
+++ app/main.py
@@ -32,6 +32,9 @@ app.include_router(admin.router)
 app.include_router(notes.router)
 app.include_router(auth.router)
 app.include_router(best_day.router)
+#--FonkSyon mete 
+app.include_router(combos.router)
+app.include_router(tickets.router)
 
 
 @app.get("/")
