e0f0bbcf85f95f815a10b0899f9f177076313227
andersonjbaptiste26 <andersonjbaptiste26@gmail.com>
2026-09-18 21:23:52 -0400

    Refactor FastAPI setup and static file serving

--- app/main.py
+++ app/main.py
@@ -4,6 +4,31 @@ from fastapi.middleware.cors import CORSMiddleware
 from .routers import matches, predictions, teams, bets, admin, notes, auth, best_day, tickets
 from .utils.cache import cache_info, invalidate_cache
 
+
+
+
+#----- Beeeeeggginnnnn ----
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+# Monter le dossier "app" pour qu'il soit accessible via l'URL /static
+# Cela permettra d'accéder à l'image via /static/jbaprono.png
+app.mount("/static", StaticFiles(directory="app"), name="static")
+
+# Route pour servir votre page HTML
+@app.get("/")
+def read_root():
+    return FileResponse("index.html") # Modifiez si votre HTML est ailleurs
+
+
+
+#----------
+######
+#######№
 app = FastAPI(
     title="JBa Prono API",
     description="Backend d'analyse statistique et prédictive des matchs de football.",
@@ -57,21 +82,3 @@ def clear_cache(response: Response, key: str = None):
     response.headers["Cache-Control"] = "no-store"
     invalidate_cache(key)
     return {"status": "cleared", "key": key or "all"}
-
-
-#----- Beeeeeggginnnnn ----
-
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
-
-app = FastAPI()
-
-# Monter le dossier "app" pour qu'il soit accessible via l'URL /static
-# Cela permettra d'accéder à l'image via /static/jbaprono.png
-app.mount("/static", StaticFiles(directory="app"), name="static")
-
-# Route pour servir votre page HTML
-@app.get("/")
-def read_root():
-    return FileResponse("index.html") # Modifiez si votre HTML est ailleurs
